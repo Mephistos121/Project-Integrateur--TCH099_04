9877898ac4a622ff28e8e6cef7886b1e9ebeb34f
Jason Yin <jasonyin2004@gmail.com>
2024-03-13 17:27:11 -0400

    Voir la liste de film - RF-001 Ajout dynamique film vedette et Ajout style css

--- siteWeb/acceuil.js
+++ siteWeb/acceuil.js
@@ -1,10 +1,18 @@
 let movies = [{ title: "Lorem ipsum", image: "https://placehold.co/300x400" },
-        { title: "Lorem ipsum", image: "https://placehold.co/300x400" },
-        { title: "Lorem ipsum", image: "https://placehold.co/300x400" },
-        { title: "Lorem ipsum", image: "https://placehold.co/300x400" }];
+{ title: "Lorem ipsum", image: "https://placehold.co/300x400" },
+{ title: "Lorem ipsum", image: "https://placehold.co/300x400" },
+{ title: "Lorem ipsum", image: "https://placehold.co/300x400" },
+{ title: "Lorem ipsum", image: "https://placehold.co/300x400" },
+{ title: "Lorem ipsum", image: "https://placehold.co/300x400" }];
+
+let featuredMovie = [{ title: "Lorem ipsum", image: "https://placehold.co/1000x400" },
+{ title: "Lorem ipsum", image: "https://placehold.co/1000x400" },
+{ title: "Lorem ipsum", image: "https://placehold.co/1000x400" }];
 
 const movieList = document.getElementById("movie_list");
+const featuredMovieList = document.getElementById("featured_movie_list");
 
+showFeaturedMovie(featuredMovie);
 showMovies(movies);
 
 function showMovies(movies) {
@@ -18,11 +26,27 @@ function showMovies(movies) {
         imgPoster.alt = movie.title;
         movieTitle.textContent = movie.title;
 
-        movieLink.appendChild(movieTitle);
         movieLink.href = "";
-
         movieLink.appendChild(imgPoster);
+        movieLink.appendChild(movieTitle);
         movieListItem.appendChild(movieLink);
         movieList.appendChild(movieListItem);
     });
 }
+
+function showFeaturedMovie(movie) {
+    movie.forEach(movie => {
+        const featuredMovieListItem = document.createElement("li");
+        const imgPoster = document.createElement("img");
+        const movieLink = document.createElement("a");
+
+        imgPoster.src = movie.image;
+        imgPoster.alt = movie.title;
+
+        movieLink.href = "";
+        movieLink.appendChild(imgPoster);
+        featuredMovieListItem.appendChild(movieLink);
+        featuredMovieList.appendChild(featuredMovieListItem);
+    });
+}
+
