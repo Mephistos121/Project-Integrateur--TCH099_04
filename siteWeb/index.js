b0c80d5c6dff70765bcf6e2540b79cfcb223e3d9
Jason Yin <jasonyin2004@gmail.com>
2024-03-13 20:29:02 -0400

    Voir la liste de film - RF-001 Ajout de css(début) et film vedette

--- siteWeb/index.js
+++ siteWeb/index.js
@@ -5,12 +5,13 @@ let movies = [{ title: "Lorem ipsum", image: "https://placehold.co/300x400" },
 { title: "Lorem ipsum", image: "https://placehold.co/300x400" },
 { title: "Lorem ipsum", image: "https://placehold.co/300x400" }];
 
-let featuredMovie = [{ title: "Lorem ipsum", image: "https://placehold.co/1000x400" },
-{ title: "Lorem ipsum", image: "https://placehold.co/1000x400" },
-{ title: "Lorem ipsum", image: "https://placehold.co/1000x400" }];
+let featuredMovie = [    { title: "Lorem ipsum", image: "https://images.unsplash.com/photo-1656464868371-602be27fd4c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" },
+{ title: "Lorem ipsum", image: "https://images.unsplash.com/photo-1657586640569-4a3d4577328c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" },
+{ title: "Lorem ipsum", image: "https://images.unsplash.com/photo-1656077217715-bdaeb06bd01f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" }];
 
 const movieList = document.getElementById("movie_list");
-const featuredMovieList = document.getElementById("featured_movie_list");
+const featuredMovieSlider = document.getElementById("featured_movie_slider");
+const featuredMovieContainer = document.getElementById("featured_movie_container");
 
 showFeaturedMovie(featuredMovie);
 showMovies(movies);
@@ -35,18 +36,23 @@ function showMovies(movies) {
 }
 
 function showFeaturedMovie(movie) {
+    let counter = 0;
     movie.forEach(movie => {
-        const featuredMovieListItem = document.createElement("li");
         const imgPoster = document.createElement("img");
         const movieLink = document.createElement("a");
 
+        const sliderNav = document.createElement("a");
+
         imgPoster.src = movie.image;
         imgPoster.alt = movie.title;
-
+        imgPoster.id = `featured_movie_${counter}`;
         movieLink.href = "";
         movieLink.appendChild(imgPoster);
-        featuredMovieListItem.appendChild(movieLink);
-        featuredMovieList.appendChild(featuredMovieListItem);
+        featuredMovieSlider.appendChild(movieLink);
+
+        sliderNav.href = `#featured_movie_${counter}`;
+        counter++;
+        featuredMovieContainer.appendChild(sliderNav);
     });
 }
 
