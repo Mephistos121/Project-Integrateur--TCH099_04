5bbecd170e9ed145490c2945d7226e6b4d7a4044
Jason Yin <jasonyin2004@gmail.com>
2024-03-13 20:29:08 -0400

    Voir la liste de film - RF-001 Ajout de css(début) et film vedette

--- siteWeb/index.js
+++ siteWeb/index.js
@@ -5,9 +5,9 @@ let movies = [{ title: "Lorem ipsum", image: "https://placehold.co/300x400" },
 { title: "Lorem ipsum", image: "https://placehold.co/300x400" },
 { title: "Lorem ipsum", image: "https://placehold.co/300x400" }];
 
-let featuredMovie = [    { title: "Lorem ipsum", image: "https://images.unsplash.com/photo-1656464868371-602be27fd4c2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" },
-{ title: "Lorem ipsum", image: "https://images.unsplash.com/photo-1657586640569-4a3d4577328c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" },
-{ title: "Lorem ipsum", image: "https://images.unsplash.com/photo-1656077217715-bdaeb06bd01f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1200&q=80" }];
+let featuredMovie = [{ title: "Lorem ipsum", image: "https://placehold.co/600x400" },
+{ title: "Lorem ipsum", image: "https://placehold.co/600x400" },
+{ title: "Lorem ipsum", image: "https://placehold.co/600x400" }];
 
 const movieList = document.getElementById("movie_list");
 const featuredMovieSlider = document.getElementById("featured_movie_slider");
