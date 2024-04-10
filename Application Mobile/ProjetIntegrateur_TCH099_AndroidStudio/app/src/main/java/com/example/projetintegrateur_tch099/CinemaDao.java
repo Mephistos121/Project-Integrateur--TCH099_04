d51e299984f32f00ca1dfba48feb7aa52ba0566a
Mephistos121 <108494472+Mephistos121@users.noreply.github.com>
2024-04-09 22:12:51 -0400

    Added Cinema Fetcher

--- Application Mobile/ProjetIntegrateur_TCH099_AndroidStudio/app/src/main/java/com/example/projetintegrateur_tch099/CinemaDao.java
+++ Application Mobile/ProjetIntegrateur_TCH099_AndroidStudio/app/src/main/java/com/example/projetintegrateur_tch099/CinemaDao.java
@@ -1,8 +1,6 @@
 package com.example.projetintegrateur_tch099;
 
 import android.content.Context;
-import android.content.Intent;
-import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -15,50 +13,47 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
-import java.util.List;
 
-public class FilmDao {
-    private static FilmDao instance = null;
-    private ArrayList<Film> films = new ArrayList<>();
+public class CinemaDao {
 
-    public static FilmDao getInstance(Context context){
-        if(instance == null){
-            instance = new FilmDao(context);
-        }
-        return instance;
-    }
+    private static CinemaDao instance = null;
+    private final ArrayList<Cinema> cinemas = new ArrayList<>();
 
-    public ArrayList<Film> getFilms() {
-        return films;
-    }
-
-    private FilmDao(Context context){
-        String url = "https://equipe500.tch099.ovh/projet4/api/films";
+    private CinemaDao(Context context) {
+        String url = "https://equipe500.tch099.ovh/projet4/api/cinemas";
 
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray jsonArray) {
-
                 for (int i = 0; i < jsonArray.length(); i++) {
                     try {
                         JSONObject jsonObject = jsonArray.getJSONObject(i);
-                        String nom_film = jsonObject.getString("nom_film");
-                        String image = jsonObject.getString("image");
-                        String description = jsonObject.getString("description");
-                        Film film = new Film(nom_film,image,description);
-                        films.add(film);
+
+                        Cinema cinema = new Cinema(jsonObject.getInt("id"), jsonObject.getString("nom_cinema"),
+                                jsonObject.getString("image"), jsonObject.getString("localisation"), context);
+                        cinemas.add(cinema);
+
+
                     } catch (JSONException e) {
                         System.out.println(e.getMessage());
                     }
+
                 }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError volleyError) {
-                //add error message
+                //Toast.makeText(MainActivity.this, volleyError.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
         RequestQueue requestQueue = VolleySingleton.getInstance(context).getRequestQueue();
         requestQueue.add(jsonArrayRequest);
+    }
+
+    public static CinemaDao getInstance(Context context) {
+        if (instance == null) {
+            instance = new CinemaDao(context);
+        }
+        return instance;
     }
 }
