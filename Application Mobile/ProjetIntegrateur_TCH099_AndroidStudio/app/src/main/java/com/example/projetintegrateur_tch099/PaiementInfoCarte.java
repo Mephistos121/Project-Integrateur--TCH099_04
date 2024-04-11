6915035c4c28ac05da512a66f3472d407eae5f8f
BeniArchitect <123586604+BeniArchitect@users.noreply.github.com>
2024-04-11 17:26:08 -0400

    Billeterie xml fully done

---
+++ Application Mobile/ProjetIntegrateur_TCH099_AndroidStudio/app/src/main/java/com/example/projetintegrateur_tch099/PaiementInfoCarte.java
@@ -0,0 +1,24 @@
+package com.example.projetintegrateur_tch099;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PaiementInfoCarte extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_paiement_info_carte);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
